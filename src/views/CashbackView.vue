6c8dda0342360fe01e51b029f5cbe67b7b03a619
hirosi <hirosi840101@gmail.com>
2024-02-22 16:27:10 +0800

    cashback頁面新增

--- src/views/CashbackView.vue
+++ src/views/CashbackView.vue
@@ -25,40 +25,42 @@
             BRL
         </div>
     </header>
-    <div class="main container-fluid">
+    <div class="main container-fluid cashbackWinsows pt-3">
         <div class="vipCard mb-3 p-3">
-            <div class="main">
+            <div class="main d-flex justify-content-between align-items-center mb-2">
                 <div class="cashMain">
-                    <img src="" alt="">
-                    <span class="current">13% Cashback</span>
+                    <img src="../assets/images/icon/money1.png" width="44" alt="">
+                    <span class="current fw-bold mx-2">13% Cashback</span>
                     <span class="max">Max:25%</span>
                 </div>
-                <div class="vipMaim vip1">
-                    <span class="iconfont icon-vip"></span>
+                <div class="vipMaim vip1 d-flex justify-content-between">
+                    <span class="iconfont icon-vip me-1">
+                        <img src="../assets/images/icon/menuIcon05.png" width="25" alt="">
+                    </span>
                     <span class="vipNum">v1</span>
                 </div>
             </div>
-            <div class="progress van-progress">
-                <span class="van-progress__portion"></span>
+            <div class="progress van-progress mb-2">
+                <span class="vanProgressPortion"></span>
             </div>
             <div class="progressTips"> 5000 Apostas to VIP2 </div>
         </div>
-        <div class="vipCash mb-3 p-3">
-            <div class="img">
-                <img src="https://6z.com/img/M-4.328863a6.png" alt="" class="cashImg">
-                <img src="https://6z.com/img/free.b8fcf912.png" alt="" class="free">
+        <div class="vipCash d-flex align-items-center mb-3 p-3">
+            <div class="img position-relative">
+                <img src="https://6z.com/img/M-4.328863a6.png" alt="" class="cashImg" width="86">
+                <img src="https://6z.com/img/free.b8fcf912.png" alt="" class="free" width="44">
             </div>
             <div class="main">
-                <div class="title">Seu Cashback Semanal</div>
-                <p>Você ganhará ??? BRL em cashback jogando esta semana.</p>
-                <div class="time">Periodo 2024-02-12 00:00:00~2024-02-18 23:59:59</div>
-                <div class="button"> Reivindicar Agora </div>
+                <div class="title fw-bold mb-1">Seu Cashback Semanal</div>
+                <p class="mb-1">Você ganhará ??? BRL em cashback jogando esta semana.</p>
+                <div class="time mb-1">Periodo 2024-02-12 00:00:00~2024-02-18 23:59:59</div>
+                <div class="button mb-2"> Reivindicar Agora </div>
                 <div class="extTime">Tempo de reivindicação 2024-02-19 06:00:00~2024-02-23 23:59:59</div>
             </div>
         </div>
         <div class="vipList mb-3 p-3">
             <h3> Status VIP Cashback </h3>
-            <table>
+            <table class="table table-bordered table-striped align-middle">
                 <thead>
                     <tr>
                         <th>VIP</th>
@@ -130,19 +132,98 @@
                 <li>5. Caso você não tenha apostado durante o período em que o cashback estava ativo ou se seus ganhos da última semana ou ganhos totais são maiores que suas perdas, você não receberá cashback.</li>
                 <li>6. Limite máximo de recompensa de cashback por pessoa por semana: 10000 BRL</li>
                 <li>7. O valor do cashback pode ser sacar diretamente ou utilizado para continuar jogando</li>
+                <li>8. O valor do Cashback Semanal não é contabilizado como aposta válida para o calculo do próximo período de distribuição.</li>
             </ul>
         </div>
     </div>
 </template>
 <style scoped>
-    .app{
-        padding-top: 4rem;
-        background-color: #ededed ;
+    .cashbackWinsows{
+        background-color: #ededed;
+        padding-bottom: 5rem;
     }
     .headerBack{background-color: var(--greenBg);}
     .headerBack .title{color: var(--yellow);}
     .headerBack .moneyBox{color: var(--fff);}
-    .vipCard{background-color: var(--yellow);}
+    .vipCard{
+        border-radius: .5rem;
+        color: #009d80;
+        background-color: var(--yellow);}
+    .vipCard .cashMain .current{
+        font-size: 1.5rem;
+    }
+    .vipCard .cashMain .max{
+        color: #383838;
+        font-size: .8rem;
+    }
+    .vipCard .vipMaim .vipNum{
+        padding: 2px 9px;
+        color: var(--fff);
+        border-radius: 3px;
+        font-size: .7rem;
+        background-color: #1a9c82;
+    }
+    .vipCard .progress{
+        height: .6rem;
+        background-color: var(--fff);
+    }
+    .vipCard .progressTips{
+        color: #383838;
+        text-align: right;
+        font-size: .7rem;
+    }
     .vipCash{background-color: var(--fff);}
-    .vipList{background-color: var(--fff);}
+    .vipCash .img .free{
+        position: absolute;
+        left: 2.5rem;
+        top: 3rem;
+    }
+    .vipCash .main .title{
+        color: #cf0202;
+        font-size: 1.3rem;
+    }
+    .vipCash .main p{
+        font-size: .9rem;
+    }
+    .vipCash .main .time{
+        color: red;
+        font-size: .65rem;
+    }
+    .vipCash .main .button{
+        text-align: center;
+        padding: .5rem 0;
+        font-size: .7rem;
+        color: var(--fff);
+        background: var(--greenGradient);
+    }
+    .vipCash .main .extTime{
+        font-size: .55rem;
+        color: grey;
+        text-align: center;
+    }
+    .vipList{
+        font-weight: bold;
+        background-color: var(--fff);}
+    .vipList h3{
+        color: grey;
+        font-size: 1rem;
+        font-weight: bold;
+    }
+    .vipList table{
+        text-align: center;
+        font-size: .9rem;
+    }
+    .vipList thead th{
+        color: #009d80;
+    }
+    .vipList table tr:nth-child(10) td{
+        color: #cf0202;
+    }
+    .vipRules h3{
+        font-weight: bold;
+        font-size: 1rem;
+    }
+    .vipRules{
+        font-size: .8rem;
+    }
 </style>
