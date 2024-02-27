d62bf17a3c3fc35aea22b7706534f2bf94ff794b
hirosi <hirosi840101@gmail.com>
2024-02-27 18:15:44 +0800

    新增轉盤(未完成)

--- src/App.vue
+++ src/App.vue
@@ -6,18 +6,18 @@
   <MainMenu></MainMenu>
   <!-- 個人選單 -->
   <ProfileWindow></ProfileWindow>
+  <Turntable></Turntable>
 </template>
 
 <script>
 import BottomMenu from './components/BottomMenu.vue'
 import MainMenu from './components/MainMenu.vue'
 import ProfileWindow from './components/ProfileWindow.vue'
-
 export default {
   components: {
     BottomMenu,
     MainMenu,
-    ProfileWindow
+    ProfileWindow,
   },
 }
 </script>
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig, loadEnv  } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import vue from '@vitejs/plugin-vue'
 
 
@@ -26,7 +27,10 @@ export default defineConfig(({ command, mode }) => {
   } else {
     return {
       plugins: [
-        vue(),        
+        vue(),
+        VitePWA({
+          
+        })  
       ],
       base:"/",
       resolve: {
