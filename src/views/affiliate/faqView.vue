c9200a61b397a30a3474db237913f331f9582f2d
hirosi <hirosi840101@gmail.com>
2024-03-07 16:27:01 +0800

    新增affiliate 頁面(未完成)

---
+++ src/views/affiliate/faqView.vue
@@ -0,0 +1,86 @@
+<template>
+    <div class="fqaContent p-2 mb-3">
+        <div class="content mb-3">
+        <h2 class="mb-2 pb-2">
+            O que é o programa de afiliados 6Z?
+        </h2>
+        <div class="text mb-2">6Z Afiliados é um programa onde oferecemos a você a oportunidade de ganhar dinheiro todos os dias, simplesmente nos dando a chance de receber mais jogadores.</div>
+        </div>
+        <div class="content mb-3">
+        <h2 class="mb-2 pb-2">
+            QUANTO CUSTA PARA PARTICIPAR?
+        </h2>
+        <div class="text mb-2">Absolutamente nada. É totalmente gratuito.</div>
+        </div>
+        <div class="content mb-3">
+        <h2 class="mb-2 pb-2">
+            Posso obter benefícios mesmo se não tiver um site? 
+        </h2>
+        <div class="text mb-2">Claro que pode. Podemos fornecer todas as ferramentas de marketing de que você precisa para promover a 6Z offline nas mídias sociais ou por e-mail.</div>
+        </div>
+        <div class="content mb-3">
+        <h2 class="mb-2 pb-2">
+            QUANTO EU GANHO? 
+        </h2>
+        <div class="text mb-2">Ganhe dinheiro indicando amigos para a 6z, você ganha sempre que um convidado seu realiza uma aposta!</div>
+        <div class="linkBtn mb-1">Regras de recompensa da comissão</div>
+        <div class="linkBtn mb-1">Regras de cálculo de comissão</div>
+        </div>
+        <div class="hintTitle mb-3">Ganhe dinheiro indicando amigos para a 6z, você ganha sempre que um convidado seu realiza uma aposta!</div>
+        <div class="imgContent mb-3">
+        <img src="../../assets/images/icon/money2.png" width="73" alt="">
+        <span>Comissão pode ser</span>
+        <input type="range" class="rangeBar ms-auto">
+        </div>
+        <div class="relationSchema">
+        <div class="mb-2 text-center fw-bold">Desenvolvimento ilimitado de indicados</div>
+        <img src="../../assets/images/icon/group.png" alt="" class="w-100 mb-5">
+        <ul>
+            <li class="mb-2">Você receberá uma porcentagem de comissão diferente toda vez que um jogador indicado por você fizer uma aposta, 
+            <span>ganhar ou perder</span>.
+            </li>
+            <li>O sistema calcula a comissão a cada 24 horas.</li>
+        </ul>
+        </div>
+        <div class="content mb-3">
+        <h2 class="mb-2 pb-2">
+            Definição
+        </h2>
+        <div class="text mb-2">Inscrições - Número de usuários registrados</div>
+        <div class="text mb-2">Novos Jogadores - Novos jogadores pagantes entre os usuários registrados de hoje</div>
+        <div class="text mb-2"> Aposta válidas em equiper[num] - Total de valor de apostas válidas provenientes de indicações e contribuições de suas equipes. num - Número total de indicações e membros de suas equipes que contribuíram com apostas válidas.</div>
+        </div>
+    </div>
+</template>
+<style scoped>
+    .fqaContent{
+        background-color: var(--fff);
+        border-radius: .5rem;
+        font-size: .9rem;
+    }
+    .fqaContent h2{
+        color: #383838;
+        font-size: 1.2rem;
+        font-weight: bold;
+        border-bottom: 1px solid #bdbdbd;
+    }
+    .fqaContent .text{
+        color: grey;
+    }
+    .fqaContent .linkBtn{
+        color: #009d80;
+    }
+    .fqaContent .imgContent{
+        display: flex;
+        align-items: center;
+    }
+    .fqaContent .imgContent span{
+        color: red;
+    }
+    .fqaContent .relationSchema ul{
+        list-style: decimal ;
+    }
+    .fqaContent .relationSchema li span{
+        color: red;
+    }
+</style>
---
+++ src/views/affiliate/referralsView.vue
@@ -0,0 +1,98 @@
+<template>
+    <div class="main px-3 referContent">
+        <div class="mb-2 pt-3">
+        <div class="time px-2 border d-flex">
+            <div class="dateTime">2024-03-01</div>
+            <div class="iconBox">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#aaa" class="bi ms-1 bi-calendar4-week" viewBox="0 0 16 16">
+                <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5M2 2a1 1 0 0 0-1 1v1h14V3a1 1 0 0 0-1-1zm13 3H1v9a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1z"/>
+                <path d="M11 7.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm-3 0a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm-2 3a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm-3 0a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5z"/>
+            </svg>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#aaa" class="bi ms-1 bi-x-lg" viewBox="0 0 16 16">
+                <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z"/>
+            </svg>
+            </div>
+        </div>
+        </div>
+        <!-- 表單 -->
+        <table class="table mb-3">
+        <thead class="text-center">
+            <tr>
+            <th class="align-middle pb-2">#</th>
+            <th class="align-middle pb-2">Conta</th>
+            <th class="align-middle pb-2">Apostas(R$)</th>
+            <th class="align-middle pb-2">Aposta equipe(R$)</th>
+            <th class="align-middle pb-2">Comissão</th>
+            </tr>
+        </thead>
+        <tbody>
+            <!-- 無資料 -->
+            <tr>
+            <td class="noData pt-3 active" colspan="5">Opa! Ainda não há dados!</td>
+            </tr>
+            <!-- 有資料 -->
+            <!-- <tr>
+            <td>2024/03/06</td>
+            <td>ccccc</td>
+            <td>99,999</td>
+            <td>ccccc</td>
+            <td>ccccc</td>
+            </tr> -->
+        </tbody>
+        </table>
+        <div class="total">
+        <div class="d-flex flex-column align-items-center">
+            <div class="num">R$ 0</div>
+            <span>Aposta válidas em equipe[0]</span>
+        </div>
+        <div class="d-flex flex-column align-items-center mx-3">
+            <div class="num">R$ 0</div>
+            <span>Comissão</span>
+        </div>
+        </div>
+    </div>
+</template>
+<style scoped>
+    .referContent{
+        background-color: var(--fff);
+        height: calc(100vh - 12rem);
+        border-radius: .3rem;
+    }
+    .referContent .time{
+        width: 43%;
+        height: 1.9rem;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        font-size: .8rem;
+    }
+    .referContent .total{
+        display: flex;
+        justify-content: flex-end;
+    }
+    .referContent .total .num{
+        font-size: 1.3rem;
+        font-weight: bold;
+    }
+    .referContent .total span{
+        color: grey;
+        font-size: .8rem;
+    }
+    .referContent table{
+        font-size: .9rem;
+    }
+    .referContent table th{
+        color: #808080;
+        font-weight: bold;
+        font-size: .8rem;
+        padding: 0;
+    }
+    .referContent .noData{
+        text-align: center;
+        font-size: .9rem;
+        color: #969799;
+        border: none;
+        display: none;
+    }
+    .referContent .noData.active{display: table-cell;}
+</style>
