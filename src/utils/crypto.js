6ac7e55cf8b3a75f84bf8dbd2b5e79a2de0d0f9e
snowbo2919715 <snowbo2919715@gmail.com>
2024-03-25 09:52:11 +0800

    代码优化

---
+++ src/utils/crypto.js
@@ -0,0 +1,32 @@
+import CryptoJS from 'crypto-js/crypto-js'
+
+const key = CryptoJS.enc.Utf8.parse('123321') // 密钥 后端提供
+const iv = CryptoJS.enc.Utf8.parse('') // 偏移量
+
+/**
+ * AES加密 ：字符串 key iv  返回base64
+ */
+export function Encrypt(word) {
+  const srcs = CryptoJS.enc.Utf8.parse(word)
+  const encrypted = CryptoJS.AES.encrypt(srcs, key, {
+    iv: iv,
+    mode: CryptoJS.mode.ECB,
+    padding: CryptoJS.pad.Pkcs7
+  })
+  return CryptoJS.enc.Base64.stringify(encrypted.ciphertext)
+}
+
+/**
+ * AES 解密 ：字符串 key iv  返回base64
+ *  */
+export function Decrypt(word) {
+  const base64 = CryptoJS.enc.Base64.parse(word)
+  const src = CryptoJS.enc.Base64.stringify(base64)
+
+  const decrypt = CryptoJS.AES.decrypt(src, key, {
+    iv: iv,
+    mode: CryptoJS.mode.ECB,
+    padding: CryptoJS.pad.Pkcs7
+  })
+  return CryptoJS.enc.Utf8.stringify(decrypt)
+}
